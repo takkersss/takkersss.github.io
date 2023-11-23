da8b9e5be636598819492e914cc31ea7c4ca24b3
takkersss <105208528+takkersss@users.noreply.github.com>
2023-11-23 17:38:32 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://onvaregarderdufilm.hugo73cr.workers.dev",
+  VITE_TMDB_READ_API_KEY: "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI2YzdjMGNmOGQyZDYzODM3Y2Y1MzQ4YThkZGY4ZjdiNiIsInN1YiI6IjY1NWY3YWRmN2RmZGE2MDBmZTY1YmE2YSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.M0uCJBcpd1V_QfRyNC9iX67UhcY5D3MMloK3j7DJgSI"
+};
